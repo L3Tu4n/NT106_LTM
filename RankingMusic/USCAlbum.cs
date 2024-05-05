3487fcd05f6bd5c79c46711f03dc503e3c1cbf32
Le Phat Anh Tuan <lpatuan27@gmail.com>
2024-05-05 21:48:38 +0700

    update-apimusic-rankmusic

---
+++ RankingMusic/USCAlbum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RankingMusic
+{
+    public partial class USCAlbum : UserControl
+    {
+        public USCAlbum()
+        {
+            InitializeComponent();
+        }
+
+        private void bunifuButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
